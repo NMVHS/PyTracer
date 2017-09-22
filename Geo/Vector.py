fb51642031e7c5e49229279a3c4bf2e480eb11a5
Shawn <shawnwangvfx@outlook.com>
2017-09-23 00:13:34 +1000

    add roation matrix

--- Geo/Vector.py
+++ Geo/Vector.py
@@ -1,7 +1,8 @@
 import math
+import numpy as np
 
 class Vector:
-	
+
 	def __init__(self,x,y,z):
 		self.x = float(x)
 		self.y = float(y)
@@ -33,9 +34,47 @@ class Vector:
 		return self.x * other.x + self.y * other.y + self.z * other.z
 
 	def cross(self,other):
-		return Vector(self.y*other.z - self.z*other.y, 
-					  self.z*other.x - self.x*other.z, 
+		return Vector(self.y*other.z - self.z*other.y,
+					  self.z*other.x - self.x*other.z,
 					  self.x*other.y - self.y*other.x)
 
 	def sqr(self):
 		return self.dot(self)
+
+	def rot(self,axis,angle,axisVec=None):
+		vectorArray = np.array([self.x,self.y,self.z])
+		#input angle is degree
+		radAngle = math.radians(angle)
+		cosA = math.cos(radAngle)
+		sinA = math.sin(radAngle)
+
+
+		if axisVec == None:
+			rotXMatrix = np.array([[1,0,0],
+								  [0,cosA,sinA],
+								  [0,-sinA,cosA]])
+
+			rotYMatrix = np.array([[cosA,0,-sinA],
+								  [0,1,0],
+								  [sinA,0,cosA]])
+
+			rotZMatrix = np.array([[cosA,sinA,0],
+								  [-sinA,cosA,0],
+								  [0,0,1]])
+
+			rotMatrix = {"X":rotXMatrix,"Z":rotZMatrix,"Y":rotYMatrix}
+			result = vectorArray.dot(rotMatrix[axis])
+
+		else:
+			Vx = axisVec.x
+			Vy = axisVec.y
+			Vz = axisVec.z
+		    #arbitrary
+			rotAMatrix = np.array([[cosA+math.pow(Vx,2)*(1-cosA),Vx*Vy*(1-cosA)+Vz*sinA,Vx*Vz*(1-cosA)-Vy*sinA],
+									[Vy*Vx*(1-cosA)-Vz*sinA,cosA+math.pow(Vy,2)*(1-cosA),Vy*Vz*(1-cosA)+Vx*sinA],
+									[Vz*Vx*(1-cosA)+Vy*sinA,Vz*Vy*(1-cosA)-Vx*sinA,cosA+math.pow(Vz,2)*(1-cosA)]])
+
+			result = vectorArray.dot(rotAMatrix)
+
+
+		return Vector(result[0],result[1],result[2])
